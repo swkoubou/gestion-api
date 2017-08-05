b37f81e07fe55a7aea72d0d7a3e2cbf3a6d301ce
URANO Ryoya <urano.works.mail@gmail.com>
2017-08-05 18:32:50 +0900

    トークンを生成する関数

--- gestion/utils.py
+++ gestion/utils.py
@@ -1,5 +1,8 @@
 """便利関数とか"""
+import base64
+import uuid
+
 
 def generate_token():
-    # まだスタブ
-    return 'N0IHdzb2MiLCJzdWIiOiJBQkNERUYiLCJhdWQiOiJJSktMTU4iLCJ'
+    """アクセストークンの生成."""
+    return base64.b64encode(uuid.uuid4().bytes).decode()
