9a6134507b8b91ae9a13b6ac24880e1999130de5
URANO Ryoya <urano.works.mail@gmail.com>
2017-08-06 11:46:42 +0900

    fixed error

--- gestion/__init__.py
+++ gestion/__init__.py
@@ -75,11 +75,11 @@ app.add_url_rule('/groups/<group_name>',
                  view_func=GroupAPI.as_view('groups'),
                  methods=['GET', 'PUT', 'DELETE'])
 app.add_url_rule('/users', 
-                 view_func=UserListAPI.as_view('groups'),
+                 view_func=UserListAPI.as_view('users'),
                  methods=['GET', 'POST'])
 app.add_url_rule('/users/me', 
-                 view_func=UserMeAPI.as_view('groups'),
+                 view_func=UserMeAPI.as_view('users_me'),
                  methods=['GET', 'PUT', 'DELETE'])
 app.add_url_rule('/users/<int:user_id>', 
-                 view_func=UserAPI.as_view('groups'),
+                 view_func=UserAPI.as_view('users_list'),
                  methods=['GET', 'PUT', 'DELETE'])
